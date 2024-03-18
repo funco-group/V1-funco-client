cf5e58b54a9f788bc397fc28bead8ed21faa1075
이태호 <leetag1163@naver.com>
2024-03-18 16:02:43 +0900

    init: 백엔드 프로젝트 생성

---
+++ Backend/funco/src/main/java/com/found_404/funco/FuncoApplication.java
@@ -0,0 +1,13 @@
+package com.found_404.funco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuncoApplication.class, args);
+    }
+
+}
---
+++ Backend/funco/src/test/java/com/found_404/funco/FuncoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.found_404.funco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
