3a7ec727ad54461d2319f0719b2229b56d68571c
이태호 <leetag1163@naver.com>
2024-03-20 17:16:20 +0900

    feat: Redis, Spring Batch 개발 세팅 구현

--- Backend/funco/src/main/java/com/found_404/funco/FuncoApplication.java
+++ Backend/funco/src/main/java/com/found_404/funco/FuncoApplication.java
@@ -1,13 +1,15 @@
 package com.found_404.funco;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class FuncoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FuncoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(FuncoApplication.class, args);
+	}
 
 }
