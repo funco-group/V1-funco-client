451961e67e98cfd0ee937eadec465ed74c935e55
이선주 <sunju5402@naver.com>
2024-04-01 14:07:36 +0900

    hot-fix: 랭킹 서버 security 예외 적용 (squash merge from branch 'BE-Develop')

--- Backend/funco/src/main/java/com/found_404/funco/global/security/SecurityConfig.java
+++ Backend/funco/src/main/java/com/found_404/funco/global/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
 			.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.formLogin(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests(auth -> auth.requestMatchers(CorsUtils::isPreFlightRequest).permitAll())
-			.authorizeHttpRequests(auth -> auth.requestMatchers("/v1/auth/*/signin")
+			.authorizeHttpRequests(auth -> auth.requestMatchers("/v1/auth/*/signin", "/v1/rank")
 				.permitAll()
 				// .requestMatchers("/**")
 				// .permitAll()
