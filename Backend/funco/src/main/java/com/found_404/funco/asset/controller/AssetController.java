a13688f5979a1e60a9e96e8abe28dcca813a7518
leetaggg <leetag1163@naver.com>
2024-04-01 19:57:38 +0900

    hot-fix: Asset 도메인 AuthenticationPrincipal로 적용

--- Backend/funco/src/main/java/com/found_404/funco/asset/controller/AssetController.java
+++ Backend/funco/src/main/java/com/found_404/funco/asset/controller/AssetController.java
@@ -1,44 +1,41 @@
 package com.found_404.funco.asset.controller;
 
-import com.found_404.funco.asset.dto.response.CashResponse;
-import com.found_404.funco.asset.dto.response.TotalAssetResponse;
-import com.found_404.funco.asset.dto.response.CryptoResponse;
-import com.found_404.funco.asset.service.AssetService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.found_404.funco.asset.dto.response.CashResponse;
+import com.found_404.funco.asset.dto.response.CryptoResponse;
+import com.found_404.funco.asset.dto.response.TotalAssetResponse;
+import com.found_404.funco.asset.service.AssetService;
+import com.found_404.funco.member.domain.Member;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/v1/asset")
 @RequiredArgsConstructor
 public class AssetController {
 
-    private final AssetService assetService;
+	private final AssetService assetService;
 
-    @GetMapping()
-    public ResponseEntity<TotalAssetResponse> getMemberTotalAsset() {
-        // 임시로 설정한 memberId
-        long memberId = 1L;
+	@GetMapping()
+	public ResponseEntity<TotalAssetResponse> getMemberTotalAsset(@AuthenticationPrincipal Member member) {
+		return ResponseEntity.ok(assetService.getMemberTotalAsset(member));
+	}
 
-        return ResponseEntity.ok(assetService.getMemberTotalAsset(memberId));
-    }
+	@GetMapping("/cash")
+	public ResponseEntity<CashResponse> getMemberCash(@AuthenticationPrincipal Member member) {
+		return ResponseEntity.ok(assetService.getMemberCash(member));
+	}
 
-    @GetMapping("/cash")
-    public ResponseEntity<CashResponse> getMemberCash(){
-        // 임시로 설정한 memberId
-        long memberId = 1L;
-
-        return ResponseEntity.ok(assetService.getMemberCash(memberId));
-    }
-
-    @GetMapping("/crypto/{ticker}")
-    public ResponseEntity<CryptoResponse> getCrypto(@PathVariable String ticker) {
-        long memberId = 1L;
-
-        return ResponseEntity.ok(assetService.getCrypto(memberId, ticker));
-    }
+	@GetMapping("/crypto/{ticker}")
+	public ResponseEntity<CryptoResponse> getCrypto(@AuthenticationPrincipal Member member,
+		@PathVariable String ticker) {
+		return ResponseEntity.ok(assetService.getCrypto(member, ticker));
+	}
 
 }
