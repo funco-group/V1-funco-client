7a13db0f8b5230de13d28dcecf1d9398e31b1df3
이선주 <sunju5402@naver.com>
2024-04-02 09:50:41 +0900

    refactor: 랭킹 스케줄러 테스트 controller 구현 (squash merge from branch 'BE-Develop')

--- Backend/funco/src/main/java/com/found_404/funco/rank/controller/RankController.java
+++ Backend/funco/src/main/java/com/found_404/funco/rank/controller/RankController.java
@@ -3,6 +3,7 @@ package com.found_404.funco.rank.controller;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +25,10 @@ public class RankController {
 	public ResponseEntity<Page<RankResponse>> getRanking(@RequestParam String type,
 		@PageableDefault(size = 10) Pageable pageable) {
 		return ResponseEntity.ok(rankService.readRanking(type, pageable));
+	}
+
+	@GetMapping("/test")
+	public ResponseEntity<?> testRanking() {
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
