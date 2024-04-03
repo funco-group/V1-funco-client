bffa108a2c93ab25ac076a9ab568a4ff2b3c3efb
이선주 <sunju5402@naver.com>
2024-04-03 22:00:09 +0900

    hot-fix: 최근 거래 코인 응답 코드에 추가

--- Backend/funco/src/main/java/com/found_404/funco/member/dto/response/MemberResponse.java
+++ Backend/funco/src/main/java/com/found_404/funco/member/dto/response/MemberResponse.java
@@ -3,6 +3,7 @@ package com.found_404.funco.member.dto.response;
 import java.util.List;
 
 import com.found_404.funco.member.dto.MemberAssetInfo;
+import com.found_404.funco.trade.dto.RecentTradedCoin;
 
 import lombok.Builder;
 
@@ -15,7 +16,7 @@ public record MemberResponse(
 	Long assetRank,
 	Long followingCashRank,
 	MemberAssetInfo memberAssetInfo,
-	List<String> topCoins,
+	List<RecentTradedCoin> topCoins,
 	Long followingCash,
 	Long followerCash,
 	Boolean isFollow
