a95945d73637129f1609fdeb92adf36af4d14eb9
이태호 <leetag1163@naver.com>
2024-04-03 14:28:11 +0900

    hot-fix: 다른 유저 일별, 월별 통계 조회 API 추가 (squash merge from branch 'BE-Develop')

--- Backend/funco/src/main/java/com/found_404/funco/statistics/controller/StatisticsController.java
+++ Backend/funco/src/main/java/com/found_404/funco/statistics/controller/StatisticsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +40,24 @@ public class StatisticsController {
 
 	@GetMapping("/startdate")
 	public ResponseEntity<StartDateResponse> getStartDate(@AuthMemberId Long memberId) {
+		return ResponseEntity.status(HttpStatus.OK).body(statisticsService.readStartDate(memberId));
+	}
+
+	@GetMapping("/daily/{memberId}")
+	public ResponseEntity<List<DailyStatisticsResponse>> getOtherDailyStatistics(@PathVariable Long memberId,
+		@RequestParam Integer year,
+		@RequestParam Integer month) {
+		return ResponseEntity.status(HttpStatus.OK).body(statisticsService.readDailyStatistics(memberId, year, month));
+	}
+
+	@GetMapping("/monthly/{memberId}")
+	public ResponseEntity<List<MonthlyStatisticsResponse>> getOtherMonthlyStatistics(@PathVariable Long memberId,
+		@RequestParam Integer year) {
+		return ResponseEntity.status(HttpStatus.OK).body(statisticsService.readMonthlyStatistics(memberId, year));
+	}
+
+	@GetMapping("/startdate/{memberId}")
+	public ResponseEntity<StartDateResponse> getOtherStartDate(@PathVariable Long memberId) {
 		return ResponseEntity.status(HttpStatus.OK).body(statisticsService.readStartDate(memberId));
 	}
 }
